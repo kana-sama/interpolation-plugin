d9c25cd2f75f39d6e4643c3eca18bad4fcbc1664
kana <andrew4chrome@gmail.com>
2022-04-14 21:58:20 +0400

    refactor

--- lib/Plugin.hs
+++ lib/Plugin.hs
@@ -8,7 +8,7 @@ import Data.Data (Data)
 import Data.Generics.Uniplate.Data (transformBi)
 import FastString (fsLit, unpackFS)
 import GHC (GhcPs, HsExpr (ExplicitList, HsApp, HsLit, HsPar, HsVar), HsLit (HsString), LHsExpr, NoExtField (NoExtField), RdrName, noLoc)
-import GhcPlugins (HsParsedModule (HsParsedModule), Plugin (parsedResultAction), RdrName, SourceText (NoSourceText), defaultPlugin, fsLit, mkRdrUnqual, mkVarOcc, noLoc, unpackFS)
+import GhcPlugins (HsParsedModule (HsParsedModule), Plugin (parsedResultAction), RdrName, SourceText (NoSourceText), defaultPlugin, fsLit, mkRdrUnqual, mkVarOcc, noLoc, unLoc, unpackFS)
 
 deriving stock instance Data HsParsedModule
 
@@ -17,18 +17,19 @@ plugin = defaultPlugin {parsedResultAction = \_ _ -> pure . transformBi f}
   where
     f (HsLit _ (HsString _ str))
       | Just template <- viewTemplate (unpackFS str) =
-        HsPar NoExtField (prettyTemplate template)
+        unLoc (prettyTemplate template)
     f other = other
 
 data TemplateItem = Str String | Var String
 
 prettyTemplate :: [TemplateItem] -> LHsExpr GhcPs
 prettyTemplate template =
-  appE (varE (rdrName "mconcat")) (listE [ofItem i | i <- template])
+  parE (appE (varE (rdrName "mconcat")) (listE [ofItem i | i <- template]))
   where
     ofItem (Str s) = stringE s
     ofItem (Var v) = varE (rdrName v)
 
+    parE x = noLoc (HsPar NoExtField x) :: LHsExpr GhcPs
     stringE s = noLoc (HsLit NoExtField (HsString NoSourceText (fsLit s))) :: LHsExpr GhcPs
     listE xs = noLoc (ExplicitList NoExtField Nothing xs) :: LHsExpr GhcPs
     varE x = noLoc (HsVar NoExtField (noLoc x)) :: LHsExpr GhcPs
